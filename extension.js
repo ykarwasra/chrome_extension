38a948a3722c487eddbdf66104b2b31620cc0ae2
ykarwasra <88074851+ykarwasra@users.noreply.github.com>
2023-09-28 10:26:45 +0530

    Update extension.js

--- extension.js
+++ extension.js
@@ -4,14 +4,14 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-// 1. Grab the SAVE TAB button and store it in a tabBtn variable
+
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-// 2. Listen for clicks on tabBtn. Log Per's LinkedIn URL to the console
+
 
 
 function render(leads) {
